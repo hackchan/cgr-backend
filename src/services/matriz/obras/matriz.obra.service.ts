54bacec6d1069f884f6d379873f890f3cb1a0c25
fabio.rojas <fabio.rojas@contraloria.gov.co>
2022-09-13 17:51:54 -0500

    ajuste filter matriz obra

--- src/services/matriz/obras/matriz.obra.service.ts
+++ src/services/matriz/obras/matriz.obra.service.ts
@@ -65,82 +65,57 @@ class MatrizObraDTO {
         options.cache = true
       }
       if (isGlobalFilter) {
-        options.where = [{ fechaSuscripcion: `${DateUtils.mixedDateToUtcDatetimeString(globalFilter)}` }]
-        // options.where = [
-        // { id: Like(`%${globalFilter}%`) },
-        // { idBpin: Like(`%${globalFilter}%`) },
-        // { idContrato: Like(`%${globalFilter}%`) },
-        // { nombreProyecto: Like(`%${globalFilter}%`) },
-        // { objetoProyecto: Like(`%${globalFilter}%`) },
-        // { unidadFuncional: Like(`%${globalFilter}%`) },
+        options.where = [
+        { id: Like(`%${globalFilter}%`) },
+        { idBpin: Like(`%${globalFilter}%`) },
+        { idContrato: Like(`%${globalFilter}%`) },
+        { nombreProyecto: Like(`%${globalFilter}%`) },
+        { objetoProyecto: Like(`%${globalFilter}%`) },
+        { unidadFuncional: Like(`%${globalFilter}%`) },
+        //{ fechaSuscripcion: `${DateUtils.mixedDateToUtcDatetimeString(globalFilter)}` },
         // { fechaSuscripcion: Like(`%${globalFilter}%`) },
         // { fechaInicio: `%${format(typeof globalFilter === 'string' ? parseISO('2019-02-11T14:00:00')(globalFilter).toUTCString() : globalFilter, 'yyyy-MM-dd')}%` },
         // { fechaProgramadaTermina: Like(`%${globalFilter}%`) },
         // { fechaTermina: Like(`%${globalFilter}%`) },
-        // { valorContratoInicial: Like(`%${globalFilter}%`) },
-        // { valorContratoFinal: Like(`%${globalFilter}%`) },
-        // { avanceFisicoProgramado: Like(`%${globalFilter}%`) },
-        // { avanceFisicoEjecutado: Like(`%${globalFilter}%`) },
-        // { avanceFinancieroEjecutado: Like(`%${globalFilter}%`) },
-        // { nroContrato: Like(`%${globalFilter}%`) },
-        // { cantidadSuspenciones: Like(`%${globalFilter}%`) },
-        // { cantidadProrrogas: Like(`%${globalFilter}%`) },
-        // { tiempoSuspenciones: Like(`%${globalFilter}%`) },
-        // { tiempoProrrogas: Like(`%${globalFilter}%`) },
-        // { cantidadAdiciones: Like(`%${globalFilter}%`) },
-        // { valorTotalAdiciones: Like(`%${globalFilter}%`) },
-        // { valorComprometido: Like(`%${globalFilter}%`) },
-        // { valorObligado: Like(`%${globalFilter}%`) },
-        // { valorPagado: Like(`%${globalFilter}%`) },
-        // { valorAnticipo: Like(`%${globalFilter}%`) },
-        // { razonSocialContratista: Like(`%${globalFilter}%`) },
-        // { idContratista: Like(`%${globalFilter}%`) },
-        // { razonSocialNuevoContratista: Like(`%${globalFilter}%`) },
-        // { idNuevoContratista: Like(`%${globalFilter}%`) },
-        // { observaciones: Like(`%${globalFilter}%`) },
-        // { linkSecop: Like(`%${globalFilter}%`) },
+        { valorContratoInicial: Like(`%${globalFilter}%`) },
+        { valorContratoFinal: Like(`%${globalFilter}%`) },
+        { avanceFisicoProgramado: Like(`%${globalFilter}%`) },
+        { avanceFisicoEjecutado: Like(`%${globalFilter}%`) },
+        { avanceFinancieroEjecutado: Like(`%${globalFilter}%`) },
+        { nroContrato: Like(`%${globalFilter}%`) },
+        { cantidadSuspenciones: Like(`%${globalFilter}%`) },
+        { cantidadProrrogas: Like(`%${globalFilter}%`) },
+        { tiempoSuspenciones: Like(`%${globalFilter}%`) },
+        { tiempoProrrogas: Like(`%${globalFilter}%`) },
+        { cantidadAdiciones: Like(`%${globalFilter}%`) },
+        { valorTotalAdiciones: Like(`%${globalFilter}%`) },
+        { valorComprometido: Like(`%${globalFilter}%`) },
+        { valorObligado: Like(`%${globalFilter}%`) },
+        { valorPagado: Like(`%${globalFilter}%`) },
+        { valorAnticipo: Like(`%${globalFilter}%`) },
+        { razonSocialContratista: Like(`%${globalFilter}%`) },
+        { idContratista: Like(`%${globalFilter}%`) },
+        { razonSocialNuevoContratista: Like(`%${globalFilter}%`) },
+        { idNuevoContratista: Like(`%${globalFilter}%`) },
+        { observaciones: Like(`%${globalFilter}%`) },
+        { linkSecop: Like(`%${globalFilter}%`) },
 
-        // { nroContratoInterventoria: Like(`%${globalFilter}%`) },
-        // { nombreInterventoria: Like(`%${globalFilter}%`) },
-        // { idInterventoria: Like(`%${globalFilter}%`) },
-        // { diaCorte: Like(`%${globalFilter}%`) },
-        // { mesCorte: Like(`%${globalFilter}%`) },
-        // { anioCorte: Like(`%${globalFilter}%`) },
+        { nroContratoInterventoria: Like(`%${globalFilter}%`) },
+        { nombreInterventoria: Like(`%${globalFilter}%`) },
+        { idInterventoria: Like(`%${globalFilter}%`) },
+        { diaCorte: Like(`%${globalFilter}%`) },
+        { mesCorte: Like(`%${globalFilter}%`) },
+        { anioCorte: Like(`%${globalFilter}%`) },
 
-        // { sector: { name: Like(`%${globalFilter}%`) } },
-        // { origen: { name: Like(`%${globalFilter}%`) } },
-        // { estado: { name: Like(`%${globalFilter}%`) } },
-        // { entidad: { name: Like(`%${globalFilter}%`) } },
-        // { municipioObra: { name: Like(`%${globalFilter}%`) } }
+        { sector: { name: Like(`%${globalFilter}%`) } },
+        { origen: { name: Like(`%${globalFilter}%`) } },
+        { estado: { name: Like(`%${globalFilter}%`) } },
+        { entidad: { name: Like(`%${globalFilter}%`) } },
+        { municipioObra: { name: Like(`%${globalFilter}%`) } }
 
-        // ]
+        ]
       }
 
-      if (isFilters) {
-        console.log('filtersColumn:', filtersColumn)
-        const pushWhere: any[] = []
-        filtersColumn.forEach((obj: any) => {
-          const bus: any = {}
-          bus[obj.id] = obj.value
-          pushWhere.push(bus)
-        })
-        options.where = pushWhere
-      }
-
-      // if (isSorting) {
-      //   const sort: any = {}
-      //   sortingColumn.forEach((obj: any) => {
-      //     sort[obj.id] = obj.desc === true ? 'DESC' : 'ASC'
-      //   })
-
-      //   options.order = sort
-      // }
-      console.log('Options-->', options)
-      // const lasOptions = {
-      //   relations: { sector: true, origen: true, estado: true, entidad: true, municipioObra: true },
-      //   where: { fechaSuscripcion: DateUtils.mixedDateToUtcDatetimeString('2022-09-05') }
-      // }
-      // console.log(lasOptions)
       const obrasList = await this.repositorioMatrizObra.find(options)
       const cantidad = await this.repositorioMatrizObra.count()
       const response = { cantidad, data: obrasList }
