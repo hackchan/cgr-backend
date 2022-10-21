e2ad3aeed601f84de5089fb18c4066b7fbea1583
fabio.rojas <fabio.rojas@contraloria.gov.co>
2022-10-21 10:12:45 -0500

    estableciendo apikey

--- src/routes/azure/azure.routes.ts
+++ src/routes/azure/azure.routes.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router, RequestHandler } from 'express'
 import multer from 'multer'
 import { setUploadFiles } from '../../controllers/azure/azure.controller'
+import { checkApiKey } from '../../middlewares/auth.handler'
 const router = Router()
 const inMemoryStorage = multer.memoryStorage()
 const uploadStrategy = multer({ storage: inMemoryStorage }).single('file')
-router.get('/', uploadStrategy, setUploadFiles as RequestHandler)
+
+router.get('/', checkApiKey, uploadStrategy, setUploadFiles as RequestHandler)
 
 export default router
