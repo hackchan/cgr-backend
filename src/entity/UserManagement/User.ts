5fde9dafcefcd313dccdb5a8f2662747da57cf9c
fabio.rojas <fabio.rojas@contraloria.gov.co>
2022-10-24 10:56:25 -0500

    ajuste al modelo de usuarios

--- src/entity/UserManagement/User.ts
+++ src/entity/UserManagement/User.ts
@@ -2,6 +2,7 @@ import { Entity, Column, ManyToOne, OneToOne, JoinColumn } from 'typeorm'
 import { UserType } from './UserType'
 import { Auth } from './Auth'
 import { Model } from './Model'
+import { EntidadControl } from '../Entidad/EntidadControl'
 @Entity('user')
 export class User extends Model {
   @Column({ nullable: false })
@@ -26,4 +27,8 @@ export class User extends Model {
   @ManyToOne(() => UserType, { nullable: false, onDelete: 'CASCADE', onUpdate: 'CASCADE', cascade: true })
   @JoinColumn({ name: 'type_id' })
     tipo: UserType
+
+  @ManyToOne(() => EntidadControl, { nullable: true, onDelete: 'SET NULL', onUpdate: 'SET NULL', cascade: true })
+  @JoinColumn({ name: 'entidad_id' })
+    entidadId: EntidadControl
 }
