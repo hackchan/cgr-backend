87f0d12de17ea0f0fc58c0c339beadce5fb2902d
fabio.rojas <fabio.rojas@contraloria.gov.co>
2022-11-17 16:27:45 -0500

    ajuste de schema

--- src/routes/matriz/ies/matriz.ies.route.ts
+++ src/routes/matriz/ies/matriz.ies.route.ts
@@ -2,14 +2,15 @@ import { Router, RequestHandler } from 'express'
 import { createMatricula, deleteMatricula, updateMatricula, getMatricula, getMatriculas } from '../../../controllers/matriz/ies/matriz.ies.controller'
 import passport from 'passport'
 import { checkRoles } from '../../../middlewares/auth.handler'
-// import { validatorHandler } from '../../../middlewares/schemas/validator.handler'
-// import { getMatrizObraSchema, createMatrizObraSchema, updateMatrizObraSchema } from '../../../middlewares/schemas/obra/matrizObra.schema'
+import { validatorHandler } from '../../../middlewares/schemas/validator.handler'
+import { getMatrizIESSchema, createMatrizIESSchema, updateMatrizIESSchema } from '../../../middlewares/schemas/ies/matriz.ies.schema'
 const router = Router()
 
 router.get('/', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD', 'INFORMACION', 'ANALISIS', 'URI'), getMatriculas as RequestHandler)
-router.get('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD', 'INFORMACION', 'ANALISIS', 'URI'), getMatricula as RequestHandler)
-router.post('/', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD'), createMatricula as RequestHandler)
+router.get('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD', 'INFORMACION', 'ANALISIS', 'URI'), validatorHandler(getMatrizIESSchema, 'params'), getMatricula as RequestHandler)
+router.post('/', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD'), validatorHandler(createMatrizIESSchema, 'body'), createMatricula as RequestHandler)
 // router.post('/upsert', updateMatricula as RequestHandler)
-router.patch('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD'), updateMatricula as RequestHandler)
+router.patch('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI', 'ENTIDAD'), validatorHandler(getMatrizIESSchema, 'params'),
+  validatorHandler(updateMatrizIESSchema, 'body'), updateMatricula as RequestHandler)
 router.delete('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), checkRoles('ADMIN', 'JEDI'), deleteMatricula as RequestHandler)
 export default router
